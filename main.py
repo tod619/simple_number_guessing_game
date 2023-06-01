5324ed5b65cf52c56c3da67a0521ef128ad104c5
tod619 <trevorodwyer619@hotmail.com>
2023-06-01 21:30:12 +0100

    create starting code for the proejct in main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Simply Number Guessing Game
 # 01/06/2023
+import random
+
+answer = random.randint(1, 100)
+
+print("Welcome to the number guessing game!!!")
+print("The computer is thinking of a number between 1 - 100")
+
+guess = int(input("Make a guess: "))
+print(guess)
+
+# print(answer)
